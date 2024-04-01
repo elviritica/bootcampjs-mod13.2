6fce840bddd103e162b8119c0fe81bbe8a2ca2f0
elviritica <e.aviles.s@gmail.com>
2024-04-01 18:57:05 +0200

    estilado terminado

--- src/pages/account-list/account-list.page.tsx
+++ src/pages/account-list/account-list.page.tsx
@@ -5,6 +5,8 @@ import classes from "./account-list.page.module.css";
 import { AccountListTableComponent } from "./components";
 import { getAccountList } from "./api";
 import { mapAccountListFromApiToVm } from "./account-list.mapper";
+import { appRoutes } from "@/core/router";
+import { Link } from "react-router-dom";
 
 
 export const AccountListPage: React.FC = () => {
@@ -19,7 +21,9 @@ export const AccountListPage: React.FC = () => {
       <div className={classes.root}>
         <div className={classes.headerContainer}>
           <h1>Mis Cuentas</h1>
-          <button>AGREGAR NUEVA CUENTA</button>
+          <button>
+            <Link to={appRoutes.createAccount}>AGREGAR NUEVA CUENTA</Link>
+          </button>
         </div>
         <AccountListTableComponent accountList={accountList} />
       </div>
